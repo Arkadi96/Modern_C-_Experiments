d6ddd633e11383a8eff0bf446241082f42c69510
Arkadi96 <arkadi.hakobyan.96@mail.ru>
2024-10-27 03:00:10 +0400

    Added line by line data write support (previously it was done byte by byte) This is neccessary for solving line repetition case.

--- kernel_space/write_to_file_hexdump/hexdump.c
+++ kernel_space/write_to_file_hexdump/hexdump.c
@@ -9,6 +9,7 @@
 #define DEVICE_NAME "hexdump"
 #define LINE_SIZE 16
 #define BUFFER_SIZE 65536
+#define DATA_SIZE LINE_SIZE + LINE_SIZE / 2
 #define OUTPUT_FILE "/tmp/loop"
 #define WRITE_KERNEL_LIMIT 0x20000
 #define IS_LITTLE_ENDIAN (*(unsigned char *)&(uint16_t){1})
@@ -28,16 +29,30 @@ static void check_kernel_write(size_t r) {
     }
 }
 
-/* This function write hex char into a file */
-static void hex_char_write(struct file* o, char* c) {
+/* This function writes hex char into a file */
+static void hex_char_write(struct file* o, char c) {
     char hexbuffer[3];
-    snprintf(hexbuffer, 3, "%02x", (unsigned char)*c);
-    kernel_write(o, hexbuffer, 2, 0);
+    size_t r;
+    snprintf(hexbuffer, 3, "%02x", (unsigned char)c);
+    r = kernel_write(o, hexbuffer, 2, 0);
+    check_kernel_write(r);
+}
+
+/* This function writes pair of hex chars into a file */
+static void hex_chars_write(struct file* o, char f, char s) {
+    size_t r = kernel_write(o, " ", 1, 0);
+    check_kernel_write(r);
+    if (IS_LITTLE_ENDIAN) {
+        hex_char_write(o, s);
+        hex_char_write(o, f);
+    } else {
+        hex_char_write(o, f);
+        hex_char_write(o, s);
+    }
 }
 
 /* This function writes hex address into a file */
-static void hex_addr_write(struct file* o, unsigned int a) {
-    static bool f = true;
+static void hex_addr_write(struct file* o, unsigned int a, bool f) {
     size_t r;
     char hex_buffer[8];
     snprintf(hex_buffer, 8, "%07x", a);
@@ -47,86 +62,48 @@ static void hex_addr_write(struct file* o, unsigned int a) {
     }
     r = kernel_write(o, hex_buffer, 7, 0);
     check_kernel_write(r);
-    f = false;
+}
+
+/* This function checks for repeating lines and returns the line in hex */
+void hex_data_write(struct file* o, size_t l, size_t s) {
+    /* Fill buffer with empty spaces */
+    char fb, sb;
+    size_t i = 0;
+    for (; i < s; i += 2) {
+        fb = local_buffer[l + i];
+        sb = local_buffer[l + i + 1];
+        hex_chars_write(o, fb, sb);
+    }
 }
 
 /* This function reads data from the buffer */
 static ssize_t driver_write(struct file *File, const char *user_buffer, size_t count, loff_t *offs) {
-    //printk("count numb : %x\n", count);
-    int to_copy, not_copied, i;
+    int to_copy;
     static unsigned int v_addr = 0;
-    short el_cnt = 0;
     unsigned int total_written = 0;
-    char fb, sb, tmp;
-    bool l = false;
-    bool t = false;
+    static bool f = true; // first iter
 
     while (count > 0) {
-        //printk("count %d, to_copy %d", count, to_copy);
         to_copy = min(count, BUFFER_SIZE);
         if (copy_from_user(local_buffer, user_buffer, to_copy)) {
             printk("Failed to copy from user buffer\n");
             return -1;
         }
-        if (t) {
-            fb = tmp;
-            sb = local_buffer[0];
-            el_cnt = 2;
-            v_addr += el_cnt;
-            hex_char_write(output_file, &sb);
-            hex_char_write(output_file, &fb);
-        }
-        int i;
-        i = (t) ? 1 : 0;
-        t = false;
-        for (i; i < to_copy; i+=2) {
-            if (v_addr % LINE_SIZE == 0) {
-                hex_addr_write(output_file, v_addr);
-            }
-            kernel_write(output_file, " ", 1, 0);
-            if (i != to_copy - 1) {
-                fb = local_buffer[i];
-                sb = local_buffer[i + 1];
-                el_cnt = 2;
-            }
-            if ((i == to_copy - 1) && (count - to_copy > 0)) {
-                t = true;
-                tmp = local_buffer[i];
-            }
-            if ((i == to_copy - 1) && (count - to_copy <= 0)) {
-                t = false;
-                fb = local_buffer[to_copy - 1];
-                sb = 0;
-                el_cnt = 1;
-            }
-            if (count - to_copy <= 0) {
-                l = true;
-            }
-            if (!t) {
-                v_addr += el_cnt;
-                hex_char_write(output_file, &sb);
-                hex_char_write(output_file, &fb);
-            }
-        }
-        if (l) {
-            if (v_addr % WRITE_KERNEL_LIMIT != 0) {
-                /* Fill the line with spaces*/
-                unsigned int p_addr = (v_addr / 16) * 16;
-                size_t r_d = LINE_SIZE - (v_addr - p_addr);
-                r_d = (r_d % 2 == 1) ? r_d - 1 : r_d;
-                int c;
-                for (c = 0; c < r_d; c += 2) {
-                    kernel_write(output_file, "     ", 5, 0);
-                }
-                hex_addr_write(output_file, v_addr);
-                v_addr = 0;
-            }
-            kernel_write(output_file, "\n", 1, 0);
+        size_t i = 0;
+        for (; i < to_copy; i += LINE_SIZE) {
+            size_t s = to_copy - i >= LINE_SIZE ? LINE_SIZE : to_copy - i;
+            hex_addr_write(output_file, v_addr, f);
+            f = false;
+            hex_data_write(output_file, i, s);
+            v_addr += s;
         }
         total_written += to_copy;
         count -= to_copy;
         user_buffer += to_copy;
     }
+    hex_addr_write(output_file, v_addr, f);
+    v_addr = 0;
+    f = true;
     return total_written;
 }
 
@@ -138,9 +115,8 @@ static int driver_open(struct inode *device_file, struct file *instance) {
         printk("Failed to open output file \n");
         return -1;
     }
-
     /* Buffer for data */
-    local_buffer = kmalloc(BUFFER_SIZE, GFP_KERNEL);
+    local_buffer = (char*)kmalloc(BUFFER_SIZE, GFP_KERNEL);
     if (!local_buffer) {
         printk("Failed to allocate local buffer\n");
         return -1;
@@ -171,28 +147,23 @@ static int __init ModuleInit(void) {
         return -1;
     }
     printk("hexdump - Device Nr. Major: %d, Minor: %d was registered!\n", dev_device_nr >> 20, dev_device_nr && 0xfffff);
-
     /* Create device class */
     if ((dev_class = class_create(THIS_MODULE, DEVICE_NAME)) == NULL) {
         printk("Device class can not be created!\n");
         goto ClassError;
     }
-
     /* Create device file */
     if (device_create(dev_class, NULL, dev_device_nr, NULL, DEVICE_NAME) == NULL) {
         printk("Device class can not be created!\n");
         goto FileError;
     }
-
     /* Initialize device file */
     cdev_init(&dev_device, &fops);
-
     /* Registering device to kernel */
     if (cdev_add(&dev_device, dev_device_nr, 1) == -1) {
         printk("Registering of device to kernel failed class can not be created!\n");
         goto AddError;
     }
-
     return 0;
 
 AddError:
@@ -219,4 +190,3 @@ module_exit(ModuleExit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Arkadi Hakobyan");
 MODULE_DESCRIPTION("LKM for hexdumping files");
-
